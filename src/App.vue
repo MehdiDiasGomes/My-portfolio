6235f341b5360e4a11927b47edd364aa552a8c0a
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-09-27 22:26:29 +0200

    ✨ (app): add app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <h1>Portfolio Mehdi DIAS GOMES</h1>
+  <div class="flex flex-col justify-center items-center">
+    <div class="md:1/2 sm:w-[80%] h-screen">
+      <Navbar />
+      <home />
+    </div>
+  </div>
+  
 </template>
 
-<script setup>
-
+<script setup lang="ts">
+import home from './pages/home.vue';
+import Navbar from './components/Navbar.vue';
 </script>
