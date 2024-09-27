bcf5959b24815f40f9c2e1555c1f6d79267ecfd5
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-09-27 22:24:27 +0200

    🧱 (all): add size tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     './src/**/*.{vue,js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      fontSize: {
+        h1: ['3rem', { lineHeight: '1.2' }], 
+        h2: ['2.25rem', { lineHeight: '1.3' }], 
+        h3: ['1.875rem', { lineHeight: '1.4' }],
+        h4: ['1.5rem', { lineHeight: '1.5' }], 
+        h5: ['1.25rem', { lineHeight: '1.6' }],
+        h6: ['1rem', { lineHeight: '1.7' }], 
+      },
+    },
   },
   plugins: [],
 }
