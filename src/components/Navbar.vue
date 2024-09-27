b0de732cace56e31b3f363c16eef3efcf8a2f5f3
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-09-27 22:25:44 +0200

    ✨ (app): add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav class="text-white flex justify-between mt-5">
+        <div>
+            <img src="../../public/img/Logo.png" alt="Logo" class="h-8 w-8" />
+        </div>
+        <div class="flex gap-5">
+            <a href="https://github.com/MehdiDiasGomes" class="hover:text-[#E5484D] duration-200"><Github /></a>
+            <a href="https://github.com/MehdiDiasGomes" class="hover:text-[#E5484D] duration-200"><Linkedin /></a>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import { Github } from 'lucide-vue-next';
+import { Linkedin } from 'lucide-vue-next';
+</script>
