f70692ede9fdaccebaa9747a74f76983aa2d5f3a
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-09-28 21:42:08 +0200

    💄 Ui(app): add active-class on navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,9 +4,9 @@
             <img src="../../public/img/Logo.png" alt="Logo" class="h-8 w-8" />
         </div>
         <div class="flex gap-4 items-center">
-            <RouterLink class="hover:text-[#E5484D] duration-200" to="/">Home</RouterLink>
-            <RouterLink class="hover:text-[#E5484D] duration-200" to="/tools">Tools</RouterLink>
-            <RouterLink class="hover:text-[#E5484D] duration-200" to="/hire">Hire me</RouterLink>
+            <RouterLink class="hover:text-[#E5484D] duration-200" active-class="font-bold text-[#E5484D]" to="/">Home</RouterLink>
+            <RouterLink class="hover:text-[#E5484D] duration-200" active-class="font-bold text-[#E5484D]" to="/tools">Tools</RouterLink>
+            <RouterLink class="hover:text-[#E5484D] duration-200" active-class="font-bold text-[#E5484D]" to="/hire">Hire me</RouterLink>
         </div>
     </nav>
 </template>
