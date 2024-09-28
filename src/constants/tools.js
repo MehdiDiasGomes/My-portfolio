dc61e3e164a35ad2d41e624a735957fd1d5089fc
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-09-28 22:45:08 +0200

    💄 ✨ Feat/Ui(app): edit and style tools page

---
+++ src/constants/tools.js
@@ -0,0 +1,40 @@
+const tools = [
+    {
+        name: "Visual Studio Code",
+        img: 'Visual_Studio_Code',
+        type: "IDE",
+        link: 'https://code.visualstudio.com/'
+    },
+    {
+        name: "Google Chrome",
+        img: 'Google_Chrome',
+        type: "Web Browser",
+        link: 'https://www.google.com/chrome/'
+    },
+    {
+        name: "Figma",
+        img: 'Figma',
+        type: "Design Tool",
+        link: 'https://www.figma.com/'
+    },
+    {
+        name: "Postman",
+        img: 'Postman',
+        type: "API Client",
+        link: 'https://www.postman.com/'
+    },
+    {
+        name: "Docker",
+        img: 'Docker',
+        type: "Containerization",
+        link: 'https://www.docker.com/'
+    },
+    {
+        name: "GitHub",
+        img: 'GitHub',
+        type: "Version Control",
+        link: 'https://github.com/'
+    },
+]
+
+export default tools;
--- src/pages/tools.vue
+++ src/pages/tools.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
-        
-    </div>
+    <h2 class="text-white text-h2">Here’s a <span class="text-[#E5484D] font-extrabold">look</span> at the tools I rely
+        on.</h2>
+        <toolsCards />
 </template>
 
-<script setup lang="ts">
-
+<script setup>
+import toolsCards from '../components/toolsCards.vue';
 </script>
