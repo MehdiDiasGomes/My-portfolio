07401f0d7b14bdf9dba3cb9e411a4dce262989eb
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-09-28 18:36:25 +0200

    ✨ Feat(app): add typewriter and social cards

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex justify-start mt-28 items-start">
+    <div class="flex justify-start items-start">
         <div class="flex flex-col gap-5">
-            <div class="flex gap-3 justify-center items-center">
-                <img src="../../public/img/Mehdi-profil.jpeg" class="w-20 h-20 object-cover rounded-full"
+            <div class="flex gap-3 items-center">
+                <img src="../../public/img/Mehdi-profil.jpeg" class="w-12 h-12 object-cover rounded-full"
                     alt="Photo de profil Mehdi">
                 <h1 class="text-h1 text-white">Mehdi DIAS GOMES</h1>
             </div>
@@ -10,6 +10,9 @@
                 <h3 ref="typewriterEl" class="text-h3 text-gray-300">
                     Développeur <span class="font-extrabold text-[#E5484D]">web</span> full-stack
                 </h3>
+            </div>
+            <div>
+                <socialCards />
             </div>
         </div>
     </div>
@@ -19,17 +22,20 @@
 import { ref, onMounted } from 'vue';
 //@ts-ignore
 import Typewriter from 'typewriter-effect/dist/core';
+//@ts-ignore
+import socialCards from '../components/socialCards.vue';
 
 const typewriterEl = ref(null); // Référence pour l'élément h3
 
 onMounted(() => {
-  const typewriter = new Typewriter(typewriterEl.value, {
-    loop: false, 
-    delay: 50, 
-  });
+    const typewriter = new Typewriter(typewriterEl.value, {
+        loop: false,
+        delay: 50,
+        html: true
+    });
 
-  typewriter
-    .typeString("Développeur web full-stack") 
-    .start(); 
+    typewriter
+        .typeString('Développeur <span class="font-extrabold text-[#E5484D]">web</span> full-stack') 
+        .start();
 });
 </script>
