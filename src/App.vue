c2cf0c715739b4e4a8cd8d2c298419e08923d912
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-09-28 21:38:37 +0200

    ✨ Feat(app): implement footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="m-auto py-10 lg:py-14 px-4 max-w-3xl w-full relative">
-    <div class="flex flex-col gap-20">
-      <Navbar />
-      <RouterView />
+  <div class="min-h-screen flex flex-col">
+    <!-- Contenu principal -->
+    <div class="m-auto py-10 lg:py-14 px-4 max-w-3xl w-full flex-grow">
+      <div class="flex flex-col gap-20">
+        <Navbar />
+        <RouterView />
+      </div>
     </div>
+    <Footer class="m-auto px-4 max-w-3xl w-full" />
   </div>
 </template>
 
 <script setup>
 import Navbar from './components/navbar.vue';
-import Home from './pages/home.vue';
+import Footer from './components/footer.vue';
 </script>
---
+++ src/components/footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <footer class="">
+        <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+            <div class="sm:flex sm:items-center sm:justify-between">
+                <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                    <img src="../../public/img/Logo.png" class="h-8" alt="Mehdi Logo" />
+                </a>
+                <ul
+                    class="flex flex-wrap items-center gap-4 text-white mb-6 text-sm font-medium">
+                    <li>
+                        <RouterLink class="hover:text-[#E5484D] duration-200" to="/home">Home</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink class="hover:text-[#E5484D] duration-200" to="/tools">Tools</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink class="hover:text-[#E5484D] duration-200" to="/hire">Hire me</RouterLink>
+                    </li>
+                </ul>
+            </div>
+            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a
+                    href="https://flowbite.com/" class="hover:underline">Mehdi DIAS GOMES™</a>. All Rights Reserved.</span>
+        </div>
+    </footer>
+</template>
+
+<script setup>
+
+</script>
