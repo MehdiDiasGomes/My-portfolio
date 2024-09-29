faececc81c07932f174b7dc9d357ebb22136b2ca
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-09-29 13:59:18 +0200

    ♻️ Refactor(app): add target on link

--- src/components/socialCards.vue
+++ src/components/socialCards.vue
@@ -3,6 +3,7 @@
       <a 
         v-for="(c, index) in _cards" 
         :key="index" 
+        target="_blank"
         :href="c.link" 
         class="flex items-center text-[0.8rem] px-4 py-2 border border-[#E5484D] hover:bg-[#E5484D] duration-300 rounded-xl gap-2 text-white"
       >
