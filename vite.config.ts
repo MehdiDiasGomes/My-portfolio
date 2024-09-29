9bb786ad830b627dbc81b189cf19c961b71c1c87
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-09-29 17:38:34 +0200

    👷 Build(app)

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 3000, // Port sur lequel le serveur sera lancé
+    open: true, // Ouvrir le navigateur automatiquement
+  },
+  build: {
+    outDir: 'dist', // Dossier de sortie pour les fichiers construits
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // Alias pour simplifier les chemins d'importation
+    },
+  },
 })
