dbeec92f97c3cb5fbd6f289d2f426335694f9ff0
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-09-29 13:58:18 +0200

    🚚 Route(app): add skills routes

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import Tools from './pages/tools.vue'
 import Home from './pages/home.vue'
 import Hire from './pages/hire.vue'
 import NotFound from './pages/notFound.vue'
+import Skills from './pages/skills.vue'
 
 export const routes = [
     { path: '/', component: Home },
     { path: '/tools', component: Tools },
+    { path: '/skills', component: Skills },
     { path: '/hire', component: Hire },
     {
         path: '/:path(.*)',
