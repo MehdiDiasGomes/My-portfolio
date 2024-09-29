87ec2f8988307da985d9886be86065988089a037
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-09-29 13:57:35 +0200

    ✨ Feat(app): add skills page and constants

---
+++ src/components/skillsCards.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <ul class="flex flex-wrap gap-5">
+            <li v-for="(s, index) in _skills" :key="index" class="flex items-center max-sm:w-full w-56">
+                <a target="_blank"
+                    class="flex w-full duration-200 hover:border-[#E5484D] p-3 border border-gray-500 rounded-xl text-white items-center gap-3"
+                    :href="s.link">
+                    <img class="w-8" :src="`../../public/img/skills/${s.img}.png`"
+                        :alt="`Image ${s.name}`">
+                    <span class="flex flex-col">
+                        {{ s.name }}
+                    </span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import skills from '../constants/skills.js'
+import { ref, onMounted } from 'vue';
+
+const _skills = ref([]);
+
+onMounted(() => {
+    _skills.value = skills;
+});
+</script>
---
+++ src/pages/skills.vue
@@ -0,0 +1,9 @@
+<template>
+    <h2 class="text-white text-h2">Here’s a <span class="text-[#E5484D] font-extrabold">look</span> at the tools I rely
+        on.</h2>
+        <skills />
+</template>
+
+<script setup>
+import skills from '../components/skillsCards.vue';
+</script>
