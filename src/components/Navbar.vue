23ef1a5b24fbd68e8a6a4b62f6377e91b3fe001b
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-09-29 13:58:54 +0200

    ✨ Feat(app): add skills on navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,6 +6,7 @@
         <div class="flex gap-4 items-center">
             <RouterLink class="hover:text-[#E5484D] duration-200" active-class="font-bold text-[#E5484D]" to="/">Home</RouterLink>
             <RouterLink class="hover:text-[#E5484D] duration-200" active-class="font-bold text-[#E5484D]" to="/tools">Tools</RouterLink>
+            <RouterLink class="hover:text-[#E5484D] duration-200" active-class="font-bold text-[#E5484D]" to="/skills">Skills</RouterLink>
             <RouterLink class="hover:text-[#E5484D] duration-200" active-class="font-bold text-[#E5484D]" to="/hire">Hire me</RouterLink>
         </div>
     </nav>
