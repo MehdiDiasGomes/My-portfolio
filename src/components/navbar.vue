626fa82babed6e0695af90404cf8f324c7c42141
Mehdi DIAS GOMES <127846260+MehdiDiasGomes@users.noreply.github.com>
2024-09-29 23:11:24 +0200

    ✨Create navbar.vue

---
+++ src/components/navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="text-white flex justify-between">
+        <div>
+            <img src="../../public/img/Logo.png" alt="Logo" class="h-8 w-8" />
+        </div>
+        <div class="flex gap-4 items-center">
+            <RouterLink class="hover:text-[#E5484D] duration-200" active-class="font-bold text-[#E5484D]" to="/">Home</RouterLink>
+            <RouterLink class="hover:text-[#E5484D] duration-200" active-class="font-bold text-[#E5484D]" to="/tools">Tools</RouterLink>
+            <RouterLink class="hover:text-[#E5484D] duration-200" active-class="font-bold text-[#E5484D]" to="/skills">Skills</RouterLink>
+            <RouterLink class="hover:text-[#E5484D] duration-200" active-class="font-bold text-[#E5484D]" to="/hire">Hire me</RouterLink>
+        </div>
+    </nav>
+</template>
